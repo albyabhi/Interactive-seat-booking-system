bff446058b5869fd2bf86bd65087d8d71f9a3100
Alby <alby4645@gmail.com>
2025-01-09 17:36:22 +0530

    5 commit

--- src/components/BookingSummary.jsx
+++ src/components/BookingSummary.jsx
@@ -9,15 +9,23 @@ const BookingSummary = ({
   error,
   remainingSeats,
 }) => {
+  const showBookingAlert = () => {
+    if (selectedSeats.length > 0) {
+      const seatsList = selectedSeats.join(", ");
+      const message = `You have selected the following seats: ${seatsList}.\nTotal cost: ₹${totalCost}.`;
+      alert(message);
+    }
+  };
+
   return (
     <div className="booking-summary">
       <h2>Booking Summary</h2>
       {selectedSeats.length > 0 ? (
         <>
-          <p>No of seats : {selectedSeats.length} </p>
-          <p>{selectedSeats.join(", ")} </p>
+          <p>No of seats: {selectedSeats.length}</p>
+          <p>{selectedSeats.join(", ")}</p>
           <p>Total Cost: ₹{totalCost}</p>
-          <button onClick={handleBooking}>Book Now</button>
+          <button onClick={showBookingAlert}>Book Now</button>
           <button onClick={handleClearSeats} className="clear-btn">
             Clear Booking
           </button>
